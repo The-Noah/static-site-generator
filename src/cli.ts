e6452c3d63851c78ecb46197f3a0fb3234e4eedb
The Noah <thenoahbz@gmail.com>
2020-05-12 15:54:12 -0500

    Add live reloading to dev server

--- src/cli.ts
+++ src/cli.ts
@@ -3,10 +3,12 @@
 import * as fs from "fs";
 import * as path from "path";
 import {createServer} from "http";
+import WebSocket from "ws";
 
 import * as staticSiteGenerator from ".";
 
 const PORT = 3000;
+const WS_PORT = 3001;
 
 switch(process.argv[2]){
   case "watch": {
@@ -28,6 +30,22 @@ switch(process.argv[2]){
   } break;
   case "dev": {
     staticSiteGenerator.log.info("starting dev server...");
+
+    const wss = new WebSocket.Server({port: WS_PORT});
+    wss.on("connection", (ws) => {
+      staticSiteGenerator.log.success("new WS connection");
+    });
+
+    const changed = (event: any, file: string) => {
+      wss.clients.forEach((client) => {
+        client.send("reload");
+      });
+    };
+
+    fs.watch(staticSiteGenerator.options.srcDir, changed);
+    staticSiteGenerator.recurseDirectory(staticSiteGenerator.options.srcDir, undefined, (dir) => {
+      fs.watch(dir, changed);
+    });
 
     createServer((req, res) => {
       let url = `${req.url}`.slice(1);
@@ -56,6 +74,18 @@ switch(process.argv[2]){
         }
 
         staticSiteGenerator.renderPage(file, staticSiteGenerator.getData(), (html) => {
+          let normalDoc = false;
+          if(html.endsWith("</body></html>")){
+            normalDoc = true;
+            html = html.substring(0, html.length - "</body></html>".length);
+          }
+
+          html += `<script>var ssgs=new WebSocket("ws://localhost:${WS_PORT}");ssgs.onmessage=function(event){if(event.data==="reload"){window.location.reload()}}</script>`;
+
+          if(normalDoc){
+            html += "</body></html>";
+          }
+
           res.write(html);
           res.end();
         });
@@ -63,8 +93,9 @@ switch(process.argv[2]){
         res.write("404 not found");
         res.end();
       }
-    }).listen(PORT);
-    staticSiteGenerator.log.success(`dev server running at http://localhost:${PORT}`);
+    }).listen(PORT, () => {
+      staticSiteGenerator.log.success(`dev server running at http://localhost:${PORT}`);
+    });
   } break;
   default:
     staticSiteGenerator.build();
