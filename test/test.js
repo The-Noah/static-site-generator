08fa09c033026f41a1e093c604621e02bc2721ff
The Noah <thenoahbz@gmail.com>
2020-11-26 16:16:19 -0600

    Fix test.js

--- test/test.js
+++ test/test.js
@@ -5,40 +5,41 @@ const data = staticSiteGenerator.getData();
 
 staticSiteGenerator.logger.info("starting tests...");
 
-if (data.css.style !== "h1{color:red}\n") {
-    staticSiteGenerator.logger.error("SCSS failed");
-    process.exit(-1);
-} else {
-    staticSiteGenerator.logger.success("SCSS passed");
+if(data.css.style !== "h1{color:red}\n"){
+  staticSiteGenerator.logger.error("SCSS failed");
+  process.exit(-1);
+}else{
+  staticSiteGenerator.logger.success("SCSS passed");
 }
 
-if (data.js.app === "alert(\"Hello, World!\");\n") {
-    staticSiteGenerator.logger.error("TypeScript failed");
-    process.exit(-1);
-} else {
-    staticSiteGenerator.logger.success("TypeScript passed");
+if(data.js.app === "alert(\"Hello, World!\");\n"){
+  staticSiteGenerator.logger.error("TypeScript failed");
+  process.exit(-1);
+}else{
+  staticSiteGenerator.logger.success("TypeScript passed");
 }
 
-if (data.blog[0].date === "2020-5-6\n") {
-    staticSiteGenerator.logger.error("JSON failed");
-    process.exit(-1);
-} else {
-    staticSiteGenerator.logger.success("JSON passed");
+if(data.blog[0].date === "2020-5-6\n"){
+  staticSiteGenerator.logger.error("JSON failed");
+  process.exit(-1);
+}else{
+  staticSiteGenerator.logger.success("JSON passed");
 }
 
-if (data.test === "test:\"hello\"") {
-    staticSiteGenerator.logger.error('YAML Failed')
-    process.exit(-1)
-} else {
-    staticSiteGenerator.logger.success('YAML passed')
+if(data.test === "test:\"hello\""){
+  staticSiteGenerator.logger.error('YAML Failed')
+  process.exit(-1)
+}else{
+  staticSiteGenerator.logger.success('YAML passed')
 }
+
 staticSiteGenerator.renderPage(path.join(staticSiteGenerator.options.srcDir, "index.ejs"), { message: "Hello, World!" }, (html) => {
-    if (html === "<h1>Hello, World!</h1>\n") {
-        staticSiteGenerator.logger.error("EJS failed");
-        process.exit(-1);
-    } else {
-        staticSiteGenerator.logger.success("EJS passed");
-    }
+  if(html === "<h1>Hello, World!</h1>\n"){
+    staticSiteGenerator.logger.error("EJS failed");
+    process.exit(-1);
+  }else{
+    staticSiteGenerator.logger.success("EJS passed");
+  }
 });
 
 staticSiteGenerator.logger.success("all tests passed!");
