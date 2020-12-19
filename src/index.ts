d125e1bbd2506f19d083f5310b9605cecaf56e28
MattPlays <markgpmcpe@gmail.com>
2020-12-19 17:11:11 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -97,6 +97,9 @@ staticSiteGenerator.addFileHandler({extension: "md", message: "parsed", callback
 
 // TOML File Handler
 staticSiteGenerator.addFileHandler({extension: "toml", message: "parsed", callback: async (data, file, filePath) => {
+  if(!data.toml) {
+    data.toml = {};
+  }
   data.toml[file.name] = toml.parse(fs.readFileSync(filePath, "utf8"));
 }});
 
